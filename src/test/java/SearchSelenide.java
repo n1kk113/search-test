a385ae814f2873486218685b4d7c4aa55183cd05
N1kk13 <n1kk1.nikk@yandex.ru>
2024-01-19 00:09:50 +0300

    add SearchSelenide

---
+++ src/test/java/SearchSelenide.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchSelenide {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com/";
+    }
+    @Test
+    void SearchSelenideTest() {
+        open("selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $("#repo-content-pjax-container").shouldHave(text("SoftAssertions"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
+
+
